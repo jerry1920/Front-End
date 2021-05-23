26b6b3c33f7e9044da4dac655aab3ecf4bc651a7
Jerry Parker <59462875+jerry1920@users.noreply.github.com>
2021-05-24 00:27:16 +0530

    complete system drawer or windows drawer tray

--- windows10_homepage/main.js
+++ windows10_homepage/main.js
@@ -1,3 +1,4 @@
+// Time And Date
 setInterval(() =>{
     const time = document.querySelector("#time");
     let date = new Date();
@@ -17,7 +18,7 @@ setInterval(() =>{
     time.textContent = hours + ":" + minutes + " " + period; 
 });
 
-
+// Start Drawer or system drawer tray hide and show
 function start_drawer(){
     var show = document.getElementById("drawer-tray");
     if(show.style.display !== "block"){
@@ -27,16 +28,18 @@ function start_drawer(){
     }
 }
 
+// left-system-menu hover show 
 function show_sidebar(){
     document.getElementById('windows-start-hover-menu').style.display = "block"; 
     document.getElementById('windows-start-hover-menu').style["boxShadow"] = "5px 0 5px -4px rgba(0,0,0,0.7)"; 
 }
 
+// left-system-menu hover hide 
 function hide_sidebar(){
     document.getElementById('windows-start-hover-menu').style.display = "none"; 
 }
 
-
+// Bing Wallpaper Folder Show Hide
 function subCate_folder(){
     var folderShow = document.getElementById('folder_show');
     var arrow = document.getElementById('down-arrow');
@@ -49,6 +52,7 @@ function subCate_folder(){
     }
 }
 
+// Chrome Apps Folder Show Hide
 function subCate_chrome_folder(){
     var folderShow = document.getElementById('chrome_sub_folder_show');
     var arrow = document.getElementById('down-arrow-chrome');
@@ -61,11 +65,51 @@ function subCate_chrome_folder(){
     }
 }
 
+// Conexant Folder Show Hide
 function subCate_Conexant_folder(){
     var folderShow = document.getElementById('conexant_sub_folder_show');
     var arrow = document.getElementById('down-arrow-conexant');
     if(folderShow.style.display == "none"){
         folderShow.style.display = "block";
+        arrow.style.transform =  'rotate(180deg)';
+    } else{
+        folderShow.style.display = "none";
+        arrow.style.transform =  'rotate(0deg)';
+    }
+}
+
+// Dolby Folder Show Hide
+function subCate_Dolby_folder(){
+    var folderShow = document.getElementById('dolby_sub_folder_show');
+    var arrow = document.getElementById('down-arrow-dolby');
+    if(folderShow.style.display == "none"){
+        folderShow.style.display = "flex";
+        arrow.style.transform =  'rotate(180deg)';
+    } else{
+        folderShow.style.display = "none";
+        arrow.style.transform =  'rotate(0deg)';
+    }
+}
+
+// Git Folder Show Hide
+function subCate_git_folder(){
+    var folderShow = document.getElementById('git_sub_folder_show');
+    var arrow = document.getElementById('down-arrow-git');
+    if(folderShow.style.display == "none"){
+        folderShow.style.display = "block";
+        arrow.style.transform =  'rotate(180deg)';
+    } else{
+        folderShow.style.display = "none";
+        arrow.style.transform =  'rotate(0deg)';
+    }
+}
+
+// Github Folder Show Hide
+function subCate_github_folder(){
+    var folderShow = document.getElementById('github_sub_folder_show');
+    var arrow = document.getElementById('down-arrow-github');
+    if(folderShow.style.display == "none"){
+        folderShow.style.display = "flex";
         arrow.style.transform =  'rotate(180deg)';
     } else{
         folderShow.style.display = "none";
