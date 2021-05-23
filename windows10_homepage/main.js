22276128d246d9fdd23cab1247b67f91dbfab560
Jerry Parker <59462875+jerry1920@users.noreply.github.com>
2021-05-24 00:58:00 +0530

    changes in time and set background wallpaper

--- windows10_homepage/main.js
+++ windows10_homepage/main.js
@@ -9,8 +9,11 @@ setInterval(() =>{
         period = "PM";
         hours = hours - 12;
     }
-    if(hours < 10){
+    if(hours < 12){
        hours = "0" + hours;
+    }
+    if(hours == 0){
+        hours = 12;
     }
     if(minutes < 10){
         minutes = "0" + minutes;
